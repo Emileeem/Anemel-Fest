dd8b0d301d5b05ced60eee48baedb0f632203d70
Emyli Quadros <babaloogames@gmail.com>
2024-04-22 10:59:56 -0300

    adicionando para backup

--- anemel/src/components/LoginComponent/index.jsx
+++ anemel/src/components/LoginComponent/index.jsx
@@ -9,6 +9,20 @@ export function LoginComponent(){
                 
                 <img src="./menu.png" className={styles.logo} />
             </nav>
+            <div className={styles.body}>
+                <div className={styles.body2}>
+                    <p className={styles.bemVindo}> BEM-VINDO(A) </p>
+                    <div className={styles.bodyTextos}>
+                        <p> Não possui uma conta?</p>
+                        <button className={styles.cadastroButton}> Cadastre-se </button>
+                    </div>
+                </div>
+                <div className={styles.loginCard}>
+                    <img src="./logo.png" className={styles.logo2} />
+                    <h1> ANEMEL FEST </h1>
+                    
+                </div>
+            </div>
         </main>
     </>
     )
