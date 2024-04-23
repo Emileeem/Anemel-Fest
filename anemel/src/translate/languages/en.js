2072aff660ce1d16ede9d1be96a91371a9b7e9dd
Emyli Quadros <babaloogames@gmail.com>
2024-04-23 10:44:45 -0300

    Adicionando tradução

---
+++ anemel/src/translate/languages/en.js
@@ -0,0 +1,18 @@
+const messages = {
+    en: {
+      translations: {
+        login: {
+          welcome: "Welcome!",
+          account: "Don't have an account?",
+          buttonCad: "Register",
+          login: "Email",
+          password: "Password",
+          remember: "Remember",
+          pass: "Forgot Password?",
+          button: "Join"
+        }
+      }
+    }
+}
+
+export { messages };
